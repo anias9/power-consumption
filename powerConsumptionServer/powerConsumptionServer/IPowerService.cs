131c6a9983266ab2a57519c0f9c59c423feefbcc
anias9 <annasekula95@gmail.com>
2021-01-17 19:12:43 +0100

    wcf

---
+++ powerConsumptionServer/powerConsumptionServer/IPowerService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace powerConsumptionServer
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IPowerService
+    {
+        [OperationContract]
+        double dailyConsuption(double price, int power, int number, int hours);
+
+        [OperationContract]
+        double monthlyConsuption(double price, int power, int number, int hours);
+
+        [OperationContract]
+        double annualConsuption(double price, int power, int number, int hours);
+
+        // TODO: Add your service operations here
+    }
+
+
+}
---
+++ powerConsumptionServer/powerConsumptionServer/PowerService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace powerConsumptionServer
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class PowerService : IPowerService
+    {
+        public double dailyConsuption(double price, int power, int number, int hours)
+        {
+            double result, powerKw;
+
+            powerKw = power / 1000.0;
+            result = hours * price * powerKw * number;
+
+            return result;
+            
+        }
+
+        public double monthlyConsuption(double price, int power, int number, int hours)
+        {
+            return dailyConsuption(price,power, number,  hours) * 30; 
+        }
+
+        public double annualConsuption(double price, int power, int number, int hours)
+        {
+            return dailyConsuption(price, power, number, hours) * 365;
+        }
+
+    }
+}
